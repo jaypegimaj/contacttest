3ce6f78d993c10dc38679f0994b1b2fd58f6fa08
jaypegimaj <48104953+jaypegimaj@users.noreply.github.com>
2020-06-07 16:31:16 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<main>
+
+
+
+<p> SEND EMAIL</p>
+<form class="contact form" action="contacttest.php" method="post">
+  <input type="text" name="name" placeholder="Full name">
+  <input type="text" name="email" placeholder="Your e-mail">
+  <input type="text" name="subject" placeholder="Subject">
+  <textarea name="message" placeholder="Message"></textarea>
+  <button type="submit" name="Submit">SEND MAIL</button>
+
+</form>
+</main>
+</body>
+</html>
